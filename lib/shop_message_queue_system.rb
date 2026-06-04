d148d31971410c9d6ec93d9a6052cc7ce4461211
Tuan Le <lctuan.0807@gmail.com>
2026-06-04 23:10:38 +0700

    init

---
+++ lib/shop_message_queue_system.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "shop_message_queue_system/version"
+
+module ShopMessageQueueSystem
+  class Error < StandardError; end
+  # Your code goes here...
+end
